f158d664befdadd2e4a16590889450d4051e099b
Mark Zhitomirski <mz@exactpro.com>
2021-11-21 17:40:37 +0300

    test WIP

--- src/test/java/com/stormpath/examples/shiro/test/ContainerIntegrationTest.java
+++ src/test/java/com/stormpath/examples/shiro/test/ContainerIntegrationTest.java
@@ -1,58 +1,77 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package com.stormpath.examples.shiro.test;
 
-import com.gargoylesoftware.htmlunit.ElementNotFoundException;
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import org.junit.Before;
-import org.junit.Ignore;
+import org.eclipse.jetty.server.LocalConnector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.Test;
 
-import java.io.IOException;
+import static org.junit.Assert.assertTrue;
 
+public class ContainerIntegrationTest {
 
-public class ContainerIntegrationTest extends AbstractContainerTest {
+    @Test
+    public void logoutThenLogin() throws Exception {
+        Server server = new Server(0);
+        LocalConnector connector = new LocalConnector(server);
+        server.addConnector(connector);
+        server.setHandler(new WebAppContext("src/main/webapp", "/"));
+        server.start();
+        assertTrue(server.isStarted());
 
-    @Before
-    public void logOut() throws IOException {
+        /* www.eclipse.org/jetty/javadoc/jetty-10/org/eclipse/jetty/servlet/ServletTester.html
+
+        ServletTester is not best practice and may be deprecated and eventually removed in future Jetty versions.
+
+        *NB* moved to (<classifier>tests), first time in jetty-servlet-9.3.0.M1-tests.jar 2014-11-03
+
+        ServletTester is a just a wrapper around a ServletContextHandler, with a LocalConnector
+        to accept HTTP/1.1 requests, so there is no value that this class adds to already existing classes.
+
+        Replace its usages with:
+
+        Server server = new Server();
+        LocalConnector connector = new LocalConnector(server);
+        server.addConnector(connector);
+        ServletContextHandler context = new ServletContextHandler(server, "/contextPath");
+        // Configure the context here.
+        server.start();
+
+        You can configure the context by adding Servlets and Filters, attributes, etc. even after it has been started.
+        Use HttpTester and LocalConnector to make HTTP/1.1 requests, in this way:
+
+        // Generate the request.
+        HttpTester.Request request = HttpTester.newRequest();
+        request.setMethod("GET");
+        request.setURI("/contextPath/servletPath");
+        request.put(HttpHeader.HOST, "localhost");
+        ByteBuffer requestBuffer = request.generate();
+
+        // Send the request buffer and get the response buffer.
+        ByteBuffer responseBuffer = connector.getResponse(requestBuffer);
+
+        // Parse the response buffer.
+        HttpTester.Response response = HttpTester.parseResponse(responseBuffer);
+        assert response.getStatus() == HttpStatus.OK_200;
+        */
+        /* Below is the old "test" code
         // Make sure we are logged out
-        final HtmlPage homePage = webClient.getPage(getBaseUri());
-        try {
+        try (WebClient webClient = new WebClient()) {
+            HtmlPage homePage = webClient.getPage("/");
             homePage.getAnchorByHref("/logout").click();
         }
-        catch (ElementNotFoundException e) {
-            //Ignore
+
+        try (WebClient webClient = new WebClient()) {
+            HtmlPage page = webClient.getPage(BaseURI + "/login.jsp");
+            HtmlForm form = page.getFormByName("loginform");
+            form.getInputByName("username").setValueAttribute("root");
+            form.getInputByName("password").setValueAttribute("secret");
+            page = form.getInputByName("submit").click();
+            // This will throw an exception if not logged in
+            page.getAnchorByHref("/logout");
         }
+        */
     }
-
-    @Ignore @Test
-    public void logIn() throws FailingHttpStatusCodeException, IOException {
-        HtmlPage page = webClient.getPage(getBaseUri() + "login.jsp");
-        HtmlForm form = page.getFormByName("loginform");
-        form.getInputByName("username").setValueAttribute("root");
-        form.getInputByName("password").setValueAttribute("secret");
-        page = form.getInputByName("submit").click();
-        // This will throw an exception if not logged in
-        page.getAnchorByHref("/logout");
-    }
-
 }
